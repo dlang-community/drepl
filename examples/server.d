0abd7abd163e634f7441b75ed02e1fb65a1e0cf4
Martin Nowak <code@dawg.eu>
2014-02-06 03:40:04 +0100

    implement web terminal

--- examples/server.d
+++ examples/server.d
@@ -1,18 +1,24 @@
 import vibe.d;
+import dawg.drepl;
 
 shared static this()
 {
     auto settings = new HTTPServerSettings;
     settings.port = 8080;
     settings.bindAddresses = ["::1", "127.0.0.1"];
+    if (getOption("bindAddress|bind", &settings.bindAddresses[0], "Sets the address used for serving."))
+        settings.bindAddresses.length = 1;
+    getOption("port|p", &settings.port, "Sets the port used for serving.");
 
     auto router = new URLRouter;
     router
         .get("/", &drepl)
         .get("*", serveStaticFiles("./public/"))
-        .get("/ws/echo", handleWebSockets(&wsEcho))
+        .get("/ws/echo", handleWebSockets(s => runSession(s, echoEngine())))
+        .get("/ws/dmd", handleWebSockets(s => runSession(s, dmdEngine())))
         ;
 
+    logInfo("serving");
     listenHTTP(settings, router);
 }
 
@@ -21,12 +27,18 @@ void drepl(HTTPServerRequest req, HTTPServerResponse res)
     res.render!"drepl.dt"();
 }
 
-void wsEcho(WebSocket sock)
+void runSession(E)(WebSocket sock, E engine)
 {
+    import std.conv : to;
+    auto intp = interpreter(engine);
+    auto resp = Json.emptyObject;
     while (sock.connected)
     {
         auto msg = sock.receiveText();
-        logInfo(msg);
-        sock.send(msg);
+        auto res = intp.interpret(msg);
+
+        resp.result = to!string(res[0]);
+        resp.text = res[1].htmlEscape();
+        sock.send((scope stream) => writeJsonString(stream, resp));
     }
 }
