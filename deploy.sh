c867739417725eea66cffbf8b691d1404553b68b
Martin Nowak <code@dawg.eu>
2014-08-29 21:13:44 +0200

    handle symlinked files in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ BUILD=${BUILD:=release}
 dub build -b ${BUILD} -c sandbox
 dub build -b ${BUILD} -c server
 
-rsync -ravz --delete --no-whole-file drepl_server drepl_sandbox examples/favicon.ico root@kvm2.dawg.eu:/home/dawg/drepl/
-rsync -avz --delete --no-whole-file drepl.service root@kvm2.dawg.eu:/usr/lib/systemd/system/
+rsync -ravzL --delete --no-whole-file drepl_server drepl_sandbox examples/favicon.ico root@kvm2.dawg.eu:/home/dawg/drepl/
+rsync -avzL --delete --no-whole-file drepl.service root@kvm2.dawg.eu:/usr/lib/systemd/system/
 ssh root@kvm2.dawg.eu 'setcap cap_net_bind_service=ep /home/dawg/drepl/drepl_server'
 ssh root@kvm2.dawg.eu 'systemctl daemon-reload && systemctl restart drepl.service && sleep 1 && systemctl status drepl.service'
